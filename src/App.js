837ac31dd1cdae4116b3686f8165ae66f21bdc37
huongbp <huong.bp.97@gmail.com>
2021-04-04 12:27:00 +0700

    create app

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+
+  const [min, setMin] = useState(0);
+  const [max, setmax] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleRandom = function () {
+    setResult(randomInt(min,max));
+  };
+
+  const onChange=function(e){
+    if(e.target.id==='min'){
+      setMin(e.target.value);
+    }else if(e.target.id==="max"){ 
+      setmax(e.target.value);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="result">
+          <p>
+            Random number: <span>{result}</span>
+          </p>
+        </div>
+        <div className="option">
+          <div>
+            <p>
+              Min
+          </p>
+            <input id="min" type="number" value={min} onChange={onChange} />
+          </div>
+          <div>
+            <p>
+              Max
+          </p>
+            <input id="max" type="number" value={max} onChange={onChange}/>
+          </div>
+        </div>
+        <div className="button">
+          <button onClick={handleRandom} >
+            Random
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function randomInt(min, max) {
+  let num = Math.random() * (max - min + 1) + min;
+  return Math.floor(num);
+}
+
+export default App;
